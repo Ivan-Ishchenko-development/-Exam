db2b7215595cab01adcd801ef56329bfd4ea62bb
zbr <zbr2dev@gmail.com>
2020-06-26 11:54:15 +0300

    BLL

---
+++ ExLayer.BLL/BusinessModels/SomthingDo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExLayer.BLL.BusinessModels
+{
+    class SomthingDo
+    {
+    }
+}
---
+++ ExLayer.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExLayer.BLL.Infrastructure
+{
+    class ServiceModule
+    {
+    }
+}
